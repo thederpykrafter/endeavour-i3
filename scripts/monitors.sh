d66d9a991fef55ec5aa43a8463730bb4591ec22d
thederpykrafter <thederpykrafter@gmail.com>
2024-12-07 18:51:09 -0500

    reverse monitor placement

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -3,7 +3,7 @@
 monitor="$(xrandr | grep "HDMI" | tail -n1 | cut -d " " -f1)"
 
 if cat /sys/class/net/enp8s0/operstate | grep "up" &> /dev/null; then
-	xrandr --output $monitor --left-of eDP-1 --auto
-else
 	xrandr --output $monitor --right-of eDP-1 --auto
+else
+	xrandr --output $monitor --left-of eDP-1 --auto
 fi
