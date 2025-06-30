24e79c22430449f77c2efdf61cf2db05928fb6e8
thederpykrafter <thederpykrafter@gmail.com>
2025-06-30 02:03:10 -0400

    use a log file that exists dork

--- scripts/start-polybar.sh
+++ scripts/start-polybar.sh
@@ -10,9 +10,9 @@ MONITORS=$(xrandr | grep " connected" | wc -l)
 while [[ $(pgrep -x polybar) ]]; do sleep 1; done
 
 # laptop bar
-polybar laptop 2>&1 | tee -a /tmp/polybar.laptop.log &
+polybar laptop 2>&1 | tee -a /tmp/polybar.log &
 disown
 
 # tv bar
-[[ $(xrandr | grep "HDMI-1-0") ]] && polybar tv 2>&1 | tee -a /tmp/polybar.tv.log &
+[[ $(xrandr | grep "HDMI-1-0") ]] && polybar tv 2>&1 | tee -a /tmp/polybar.log &
 disown
