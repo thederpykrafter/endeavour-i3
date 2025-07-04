437ddfa3581c9d84549aca21da70b3b2ca321e0d
thederpykrafter <thederpykrafter@gmail.com>
2025-07-04 02:09:20 -0400

    TODO: oomox gtk and qt

--- scripts/random-wallpaper.sh
+++ scripts/random-wallpaper.sh
@@ -10,6 +10,10 @@ fi
 # apply wallpaper and generate theme
 wal --recursive -i $wallpapers -a "0.8"
 
+# reload oomox theme
+# exec_always --no-startup-id sleep 2 && themix-multi-export ~/.config/oomox/export_config/multi_export_deault.json
+# exec_always --no-startup-id sleep 2 && xrdb ~/.Xresources_themes/oomox-xresources-reverse
+
 # reload pywal themes
 ~/.config/polybar/wal-polybar.py -t ~/.config/polybar/config.template
 
